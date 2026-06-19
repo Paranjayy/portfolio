8e6d2f3ac6c3375392140724d86abf597d1efe1b
paranjay <kparanjay245@gmail.com>
2026-06-19 21:03:16 +0530

    feat: apply wide layout, fix lint warnings

--- src/types/next-global.d.ts
+++ src/types/next-global.d.ts
@@ -1,5 +1,6 @@
 declare global {
-  type PageProps<T extends string = string> = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  type PageProps<_T extends string = string> = {
     params: Promise<Record<string, string>>
     searchParams?: Promise<Record<string, string | string[] | undefined>>
   }
